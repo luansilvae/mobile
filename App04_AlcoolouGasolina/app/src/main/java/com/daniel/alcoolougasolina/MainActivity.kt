36c2654ee806e577534381844e95a92887ab1192
Guilherme G. A. Teodoro <70112245+guigran@users.noreply.github.com>
2021-06-24 20:02:40 -0300

    Inserido exception -
    Qualquer coisa que não seja um valor válido informa mensagem de valor inválido.

--- App04_AlcoolouGasolina/app/src/main/java/com/daniel/alcoolougasolina/MainActivity.kt
+++ App04_AlcoolouGasolina/app/src/main/java/com/daniel/alcoolougasolina/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +16,12 @@ class MainActivity : AppCompatActivity() {
         var resultadoCalculo : String = ""
         var valorAlcool = findViewById(R.id.Alcool) as EditText
         var valorGas = findViewById(R.id.Gasolina) as EditText
+        var resultado = findViewById(R.id.resultado) as TextView;
+       try {
+
+
         var a : Float =  valorAlcool.text.toString().toFloat()
         var g : Float =  valorGas.text.toString().toFloat()
-
         var r : Float = (a/g)
 
         if (r > 0.7){
@@ -25,7 +29,13 @@ class MainActivity : AppCompatActivity() {
         }   else{
             resultadoCalculo = "Utilize Alcool!"
         }
-        var resultado = findViewById(R.id.resultado) as TextView;
+
         resultado.setText(resultadoCalculo);
+       }
+       catch (e: Exception){
+
+           resultado.setText("Sem dados ")
+
+       }
     }
 }
---
+++ Aula03_Tabuada/app/build/generated/source/buildConfig/debug/com/luan/aula03_tabuada/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.luan.aula03_tabuada;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.luan.aula03_tabuada";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
