b62966613aa4f9db361f1b49995de7107d0fa1d4
Oliveld <62215365+Oliveld@users.noreply.github.com>
2021-06-26 13:37:00 -0300

    App listagem de usuarios

---
+++ App06_ListagemUsuarios/app/src/main/java/com/daniel/app06_listagemusuarios/Senioridade.kt
@@ -0,0 +1,7 @@
+package com.daniel.app06_listagemusuarios
+
+enum class Senioridade(val nome: String) {
+    SENIOR("Senior"),
+    PLENO("Pleno"),
+    JUNIOR("Junior")
+}
---
+++ App06_ListagemUsuarios/app/src/main/java/com/daniel/app06_listagemusuarios/Stack.kt
@@ -0,0 +1,7 @@
+package com.daniel.app06_listagemusuarios
+
+enum class Stack(val nome: String) {
+    FRONTEND("Frontend"),
+    BACKEND("Backend"),
+    FULLSTACK("Fullstack")
+}
